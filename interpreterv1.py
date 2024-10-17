610c7c2cffb27569ff2d2919ab167b133fc45dd6
Yingshi Y <72583571+yingshiye@users.noreply.github.com>
2024-10-17 03:17:48 -0700

    figure out how to get the program node, but don't know how to get the function node

---
+++ interpreterv1.py
@@ -0,0 +1,50 @@
+from intbase import InterpreterBase
+from brewparse import parse_program
+
+class Interpreter(InterpreterBase):
+
+    def __init__(self, console_output=True, inp=None, trace_output=False):
+        super().__init__(console_output, inp)   # call InterpreterBase's constructor
+
+    def run(self, program):
+        parsed_program = parse_program(program)
+        function_code = parsed_program.elem_type
+        print(function_code)
+        b = parsed_program.dict
+        print(b)
+        c = parsed_program.get()
+        print(c)
+        # function_code = get_main_function_code(parsed_program)
+        # print(parsed_program)
+        # print(Element.get( 'functions'))
+
+        # this.variable_name_to_value = Map()  # dict to hold variables
+		# main_func_node = get_main_func_node(ast)
+		# run_func(main_func_node)
+
+    # def run_function(self, func_Code):
+    
+    # def run_statment(self, statement_node):
+
+    # Note: parsed_program is Element, that has 
+    # <Element>.elem_type -> if value is program, it means this node is program node 
+        # if value is func -> funciton node
+    # <Element>.dict contains key 
+    # <Element>.get(<key>)
+
+
+
+
+
+
+
+program_source = """func main() {
+    var x;
+    x = 5 + 6;
+    print("The sum is: ", x);
+}
+"""
+
+
+a = Interpreter
+a.run(a, program_source)
