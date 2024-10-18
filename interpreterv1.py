d984f8dcd43c3eb435d8221ef1cae29a61d045c6
Yingshi Y <72583571+yingshiye@users.noreply.github.com>
2024-10-17 21:31:52 -0700

    def evaluate_expression for assignemnt

--- interpreterv1.py
+++ interpreterv1.py
@@ -8,6 +8,8 @@ class Interpreter(InterpreterBase):
     def __init__(self, console_output=True, inp=None, trace_output=False):
         super().__init__(console_output, inp)   # call InterpreterBase's constructor
 
+    def evaluate_expression(self, expression):
+        pass
 
     def run_fcall(self, fcall_Node): #print() and inputi()
         if (fcall_Node.elem_type == "print"):
@@ -18,17 +20,23 @@ class Interpreter(InterpreterBase):
     def run_statement(self, statement_node):
         if (statement_node.elem_type == 'vardef'): # variable definition
             self.variables.append((statement_node.dict.get('name'), None))
-            print(self.variables)
+            # print(self.variables)
         if (statement_node.elem_type == '='):
-            statement_node
+            # print(statement_node.dict.get('name', []))
+            for var in self.variables: 
+                if (var[0] == statement_node.dict.get('name', [])): # find variable
+                    expression = statement_node.dict.get('expression', []) #get the expression arguement for the variables 
+                    for express in expression:
+                        self.evaluate_expression(self, express)
+                # else , which is the variable not exist
         if (statement_node.elem_type == 'fcall'):
             self.run_fcall(self, statement_node)
 
     def run_function(self, func_Code): 
-        print(func_Code)
+        # print(func_Code)
         statements_Node = func_Code.dict.get('statements', [])
         for statements in statements_Node: 
-            print(statements.elem_type)
+            # print(statements.elem_type)
             self.run_statement(self, statements)
             
 
@@ -80,7 +88,7 @@ class Interpreter(InterpreterBase):
 
 program_source = """func main() {
     var x;
-    x = 5 + 6;
+    x = 5;
     print("The sum is: ", x);
 }
 """
